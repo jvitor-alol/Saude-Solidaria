e772628e4aafe2d15ef88cc5afe329f14a0247f9
João Vitor Alves <74667067+jvitor-alol@users.noreply.github.com>
2024-07-16 20:31:18 -0300

    `test`: primeiro teste funcionando com pytest

--- src/tests/test_db_connection.py
+++ src/tests/test_db_connection.py
@@ -1,16 +1,18 @@
-from src import create_app, db
+from app import create_app, db
 from sqlalchemy.sql import text
 
 
 def test_database_connection():
-    app = create_app(config='development')
+    app = create_app(config='testing')
     with app.app_context():
         try:
+            app.logger.info("Teste de conexão com db")
             with db.engine.connect() as connection:
                 connection.execute(text('SELECT 1'))
-            print('Conexão com o banco de dados estabelecida com sucesso!')
+            app.logger.info(
+                "Conexão com o banco de dados estabelecida com sucesso!")
         except Exception as e:
-            print(f'Erro ao conectar ao banco de dados: {str(e)}')
+            app.logger.info(f"Erro ao conectar ao banco de dados: {str(e)}")
 
 
 if __name__ == '__main__':
