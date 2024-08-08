b8abe1aa920c43e57223da6429d22b26a61b1f41
João Vitor Alves <74667067+jvitor-alol@users.noreply.github.com>
2024-08-08 00:06:06 -0300

    `feat`(controllers): função para upload de imagens
    - Adiciona função `salvar_imagem_temporario` que faz o upload de imagens
      para uma pasta temporária no servidor.
    - Adiciona uma constante em config.py para recuperar o caminho absoluto
      do diretório src no sistema.

--- src/app/controllers/__init__.py
+++ src/app/controllers/__init__.py
@@ -1,10 +1,12 @@
 from .auth_controller import login_user_controller
 from .register_controller import register_user
 from .account_controller import update_user, get_user_data
+from .uploads import salvar_imagem_temporario
 
 __all__ = [
     'login_user_controller',
     'register_user',
     'update_user',
-    'get_user_data'
+    'get_user_data',
+    'salvar_imagem_temporario'
 ]
--- src/app/controllers/uploads.py
+++ src/app/controllers/uploads.py
@@ -1,1 +1,25 @@
+import os
+import secrets
+from typing import Any
+
+from ..config import BASE_DIR
+
+TEMP_IMAGES_DIR = 'static/images/tmp'
+TEMP_IMAGES_DIR_ABS = os.path.join(BASE_DIR, TEMP_IMAGES_DIR)
+
+
+def salvar_imagem_temporario(form_picture: Any) -> str:
+    random_hex = secrets.token_hex(8)
+    file_extension = os.path.splitext(form_picture.filename)[1]
+
+    temp_file_name = os.path.join(
+        TEMP_IMAGES_DIR, (random_hex + file_extension))
+    temp_file_path = os.path.join(BASE_DIR, temp_file_name)
+
+    if not os.path.exists(TEMP_IMAGES_DIR_ABS):
+        os.mkdir(TEMP_IMAGES_DIR_ABS)
+
+    form_picture.save(temp_file_path)
+    return temp_file_name
+
 # TODO: Criar a lógica de upload de imagens para o S3
