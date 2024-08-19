46576bd674315deea7a582e74fcbfc1d5c21f10f
João Vitor Alves <74667067+jvitor-alol@users.noreply.github.com>
2024-08-19 16:32:08 -0300

    `refactor`(forms): add const especialidades

---
+++ src/app/forms/helpers.py
@@ -0,0 +1,12 @@
+ESPECIALIDADES = [
+    ("clinica geral", "Clínica Geral / Medicina Interna"),
+    ("pediatria", "Pediatria"),
+    ("ginecologia", "Ginecologia"),
+    ("ortopedia", "Ortopedia"),
+    ("dermatologia", "Dermatologia"),
+    ("cardiologia", "Cardiologia"),
+    ("neurologia", "Neurologia"),
+    ("oftalmologia", "Oftalmologia"),
+    ("psiquiatria", "Psiquiatria"),
+    ("oncologia", "Oncologia")
+]
