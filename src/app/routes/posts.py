6e327028dc394669219e2016389fbbbd26c0dccb
João Vitor Alves <74667067+jvitor-alol@users.noreply.github.com>
2024-08-23 08:40:23 -0300

    `fix`: rota para exclusão de posts e seus testes
    Corrige erros na exclusão de postagens e refatora a rota para o endpoint
    de `/post`.

--- src/app/routes/posts.py
+++ src/app/routes/posts.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, render_template, redirect, url_for, flash
+from flask import Blueprint, render_template, redirect, url_for, flash, abort
 from flask_login import current_user, login_required
 
+from ..extensions import db
+from ..models import Post
 from ..forms import PostForm
 from ..controllers import new_post_controller
 
@@ -20,3 +22,17 @@ def new_post():
         return redirect(url_for('views.home'))
 
     return render_template('post.html', form=form)
+
+
+@posts.route('/<int:post_id>/delete', methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.autor != current_user:
+        abort(403)
+
+    db.session.delete(post)
+    db.session.commit()
+
+    flash('Postagem deletada', 'success')
+    return redirect(url_for('views.home'))
