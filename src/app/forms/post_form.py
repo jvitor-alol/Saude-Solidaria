a9d7e0e8149b5e0e66d956c6a32eaa696c7dd5d6
João Vitor Alves <74667067+jvitor-alol@users.noreply.github.com>
2024-09-09 22:14:54 -0300

    `feat`: add campo de imagem no form de posts

--- src/app/forms/post_form.py
+++ src/app/forms/post_form.py
@@ -1,18 +1,22 @@
 from flask_wtf import FlaskForm
 from flask_ckeditor import CKEditorField
 from wtforms import StringField, SelectField, SubmitField
-from wtforms.validators import DataRequired, Length
+from flask_wtf.file import FileAllowed, FileField
+from wtforms.validators import DataRequired, Length, Optional
 
 from .helpers import ESPECIALIDADES
 
 
 class PostForm(FlaskForm):
     titulo = StringField(
-        "Titulo", validators=[DataRequired(), Length(min=10, max=255)],
+        "Titulo", validators=[DataRequired(), Length(max=255)],
         description="Digite o título da postagem")
     categoria = SelectField(
         "Categoria", validators=[DataRequired()],
         choices=ESPECIALIDADES)
+    foto_cover = FileField(
+        "Imagem de capa",
+        validators=[Optional(), FileAllowed(['jpg', 'png', 'jpeg'])])
     conteudo = CKEditorField(
         "Conteúdo", validators=[DataRequired()],
         description="Insira o texto aqui...")
